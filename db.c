fae6c3b194661545a63788bad56e37877abf4e29
Fernando Santos <fernandohenrique2000@gmail.com>
2024-06-06 00:07:51 +0100

    remove inports

--- db.c
+++ db.c
@@ -1,10 +1,7 @@
-#include <fcntl.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 typedef struct {
   char *buffer;
